3b73e026080a3634e0cbee8a6ea71b75491dd7e9
mikaelmeulle <mikael.meulle@leadingfellows.com>
2017-08-03 07:46:54 +0000

    added keyboard shortcuts

--- js/owl.js
+++ js/owl.js
@@ -2,10 +2,18 @@
     Drupal.behaviors.owl = {
         attach: function (context, settings) {
             $('.owl-slider-wrapper', context).each(function () {
-                var $this = $(this);
-                var $this_settings = $.parseJSON($this.attr('data-settings'));
-                $this.owlCarousel($this_settings);
+		    var $this = $(this);
+		    var $this_settings = $.parseJSON($this.attr('data-settings'));
+		    $this.owlCarousel($this_settings);
+		    $(document.documentElement).keyup(function (event) {
+			    if (event.keyCode == 37) {
+				    $this.trigger('owl.prev');
+			    } 
+			    else if (event.keyCode == 39) {
+				    $this.trigger('owl.next');
+			    }
 
+		    });
             });
 
         }
